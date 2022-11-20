272ab45056b6b4085850592aabfde10bbe11c0aa
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-11-20 23:21:43 +0530

    hvhjhj

--- act/src/Components/SmartSearch.jsx
+++ act/src/Components/SmartSearch.jsx
@@ -7,15 +7,11 @@ var data2 = require("../db.json");
 
 
 const SmartSearch = () => {
-<<<<<<< HEAD
   const [searITemss,setSearchItems] = useState([{full_name:""}])
   // const [searITemss,setSearchItems] = useState([{full_name:""}])
   const stateName  = JSON.parse(localStorage.getItem("statesName"))
 
   console.log('data2[stateName]', data2[stateName]);
-=======
-    
->>>>>>> 9ce1361726bdecc700e2c4e1e3cc8c481bcba391
 
     // const people = [
     //     "bob",
@@ -29,13 +25,7 @@ const SmartSearch = () => {
     // ]
     
     const [value, setValue] = useState("");
-<<<<<<< HEAD
     
-=======
-    const stateName  = JSON.parse(localStorage.getItem("statesName")
-    
-    )
->>>>>>> 9ce1361726bdecc700e2c4e1e3cc8c481bcba391
     console.log("selected-city",stateName)
 
   const onChange = (event) => {
@@ -64,12 +54,7 @@ const SmartSearch = () => {
         />
          <div className="dropdown">
         
-<<<<<<< HEAD
           {data2[stateName]?.filter((item) => {
-=======
-          {data
-            .filter((item) => {
->>>>>>> 9ce1361726bdecc700e2c4e1e3cc8c481bcba391
               const searchTerm = value.toLowerCase();
               const fullName = item.full_name.toLowerCase();
 
